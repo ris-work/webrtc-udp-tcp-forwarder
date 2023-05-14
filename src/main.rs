97d0314797c70e1dfc8da3223d72964e8764a934
chromebook <chromebook@noemail.net>
2023-05-14 14:52:06 +0000

    Import JSON parser dependencies; TOML is going well.
    FossilOrigin-Name: 17f34f599edbb18e88552bd8322bb4839c2fa0b19d961dc25cd3d984b2165ef8

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ use std::env;
 use std::fs::read_to_string;
 use std::process::exit;
 use serde::Deserialize;
+use std::net::UdpSocket;
+use std::net::TcpStream;
 #[derive(Deserialize)]
 struct Config {
     Type: String,
     WebRTCMode: String,
     Address: Option<String>,
     Port: Option<String>,
-    ICEServers: Vec<String>
+    ICEServers: Vec<String>,
+    Ordered: Option<bool>
 }
 #[derive(Deserialize)]
 struct ICEServer {
@@ -17,7 +20,14 @@ struct ICEServer {
     Username: Option<String>,
     Credential: Option<String>
 }
+#[derive(Deserialize)]
+struct WebRTC_Status {
+    Status: String,
+    SDP_Params: Option<String>,
+    SRTP_Params: Option<String>
+}
 fn main() {
+    env_logger::init();
     let mut arg_counter: usize = 0;
     let mut arg: std::env::Args;
     arg = env::args();
@@ -25,6 +35,7 @@ fn main() {
     info!("Arguments received: {arg_counter}");
     if (arg_counter != 2) {
         println!{"Expecting exactly one argument, the TOML file with connection parameters."}
+        exit(2);
     }
     let TOML_file_name = env::args().nth(1).unwrap();
     info!{"Reading from: {}", TOML_file_name};
@@ -36,5 +47,8 @@ fn main() {
     }
     info!{"{}", TOML_file_contents};
     let config : Config = toml::from_str(&TOML_file_contents).unwrap();
-    println!("Hello, world!");
+    info!("Configuration: type: {}", config.Type);
+    if(config.Type=="UDP"){
+        info!{"UDP socket requested"};
+    }
 }
