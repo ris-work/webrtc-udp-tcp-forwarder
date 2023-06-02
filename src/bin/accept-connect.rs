c3b844c1f404abfad34608aebf770e60558a30bc
rishi <oss@rishikeshan.com>
2023-06-02 10:24:27 +0000

    Better error handling (🛑⏹).
    FossilOrigin-Name: 0c6a9d0e1d0eaa556990e29190e22981ad2fabf85021504d99ec54f2f0b096d2

--- src/bin/accept-connect.rs
+++ src/bin/accept-connect.rs
@@ -12,7 +12,7 @@ use base64::Engine;
 use bytes::Bytes;
 use futures::executor::block_on;
 use lazy_static::lazy_static;
-use log::{debug, error, info, warn};
+use log::{trace, debug, error, info, warn};
 use parking_lot::Mutex;
 use serde::Deserialize;
 use std::env;
@@ -337,25 +337,46 @@ async fn configure_send_receive_tcp(
                                 let d1=d1.clone();
                                 let mut buf = [0; 65507];
                                 let amt = ClonedSocketRecv
-                                    .read(&mut buf)
-                                    .expect("Unable to read or save to the buffer");
-                                debug! {"{:?}", &buf[0..amt]};
-                                let written_bytes = block_on(d2.send(&Bytes::copy_from_slice(&buf[0..amt])))
-                                    .expect(&format! {"DataConnection {}: unable to send.", d1.label()});
-                                debug!{"Written!"};
+                                    .read(&mut buf);
+                                match (amt){
+
+                                    Ok(amt) => {
+                                        debug! {"{:?}", &buf[0..amt]};
+                                        let written_bytes = block_on(d2.send(&Bytes::copy_from_slice(&buf[0..amt])))
+                                            .expect(&format! {"DataConnection {}: unable to send.", d1.label()});
+                                        debug!{"Written!"};
+                                    },
+                                    Err(E) => {
+                                        info!{"OtherSocket: Connection closed."};
+                                        info!{"{:?}", E};
+                                        break;
+                                    }
+                                }
                             }});
                     })
                 }}));
 
 
                 // Register text message handling
-                d.on_message(Box::new(move |msg: DataChannelMessage| {
+                d.on_message(Box::new({let d=d.clone();
+                                      move |msg: DataChannelMessage| {
                     let msg = msg.data.to_vec();
                     debug!("Message from DataChannel '{d_label}': '{msg:?}'");
                     if (CAN_RECV.load(Ordering::Relaxed)){
                         let (mut ClonedSocketSend) = (ClonedSocketSend.try_clone().expect(""));
-                        ClonedSocketSend.write(&msg).expect("Unable to write data.");
-                        ClonedSocketSend.flush().expect("Unable to flush the stream.");
+                        match(
+                            ClonedSocketSend.write(&msg)
+                            )
+                        {
+                            Ok(amt) => {
+                                trace!{"Written {} bytes.", amt};
+                                ClonedSocketSend.flush().expect("Unable to flush the stream.");
+                            },
+                            Err(E) => {
+                                warn!("DataChannel: Unable to write data.");
+                                block_on(d.close());
+                            }
+                        }
                     }
                     else {
                         if (OtherSocketSendBuf.lock().len() + msg.len() > MaxOtherSocketSendBufSize) {
@@ -370,7 +391,7 @@ async fn configure_send_receive_tcp(
                         }
                     }
                     Box::pin(async {})
-                }));
+                }}));
             }
         })
     }));
