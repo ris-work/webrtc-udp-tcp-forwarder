1fa488232d749c89e32b520ba282e9eafea41cd5
rishi <rishi@noemail.net>
2023-06-13 13:09:00 +0000

    Merge better, working code (🔗)...
    FossilOrigin-Name: daad38b49e7d033c785783ef1eb9927d5a55cdef6f202b8318c8ba75555febd4

--- misc/offer/demo.js
+++ misc/offer/demo.js
@@ -6,12 +6,12 @@
 const pc = new RTCPeerConnection({
 	iceServers: [
 		{
-			urls: 'stun:stun.l.google.com:19302'
+			urls: ['stun:stun.l.google.com:19302', 'stun:vz.al']
 		}
 	]
 })
 const log = msg => {
-	document.getElementById('logs').innerHTML += msg + '<br>'
+	document.getElementById('logs').innerHTML += "<span class='time'>["+(new Date(Date.now())).toISOString()+"]</span> " + msg + '<br>'
 }
 
 
@@ -31,6 +31,7 @@ pc.onnegotiationneeded = e =>
 
 window.startSession = () => {
 	const sd = document.getElementById('remoteSessionDescription').value
+	const decoded = document.getElementById('decoded')
 	//dc = pc.createDataChannel('data')
 	pc.ondatachannel = function(e){
 		log("Ondatachannel event.")
@@ -38,19 +39,20 @@ window.startSession = () => {
 		dc.onclose = () => console.log('sendChannel has closed')
 		dc.onopen = () => console.log('sendChannel has opened')
 		dc.onmessage = async function(e){let x;
-		try{
-			if(e.data.text) {x = (await e.data.text())}
-			else {x = (new TextDecoder).decode(new Uint8Array(e.data)); }
-		}
-		catch (err) {console.log(err)};
-		log(`Message: '${dc.label}' receives '${x}'`)
+			try{
+				if(e.data.text) {x = (await e.data.text())}
+				else {x = (new TextDecoder).decode(new Uint8Array(e.data)); }
+			}
+			catch (err) {console.log(err)};
+			document.getElementById("messages_cons").innerHTML += x;
+			log(`Message: '${dc.label}' receives '${x}'`)
 		}
 		window.sendMessage = () => {
 			const message = document.getElementById('message').value
 			if (message === '') {
 				return alert('Message must not be empty')
 			}
-		
+
 			dc.send(message)
 		}
 	}
@@ -59,6 +61,7 @@ window.startSession = () => {
 	}
 	try {
 		console.log((new RTCSessionDescription (JSON.parse(atob(sd)))));
+		decoded.innerHTML=JSON.stringify(JSON.parse(atob(sd)), null, "\t").replaceAll("\\r\\n", "\n\t\t");
 		pc.setRemoteDescription(new RTCSessionDescription (JSON.parse(atob(sd)))).then(log).catch(log);
 		pc.createAnswer().then((x)=>{pc.setLocalDescription(x); console.log(x)});
 	} catch (e) {
