e7129f9223e927bbfe27cb131fcc64add35c9c33
rishi <oss@rishikeshan.com>
2024-02-28 02:48:30 +0000

    Change ld (fix the unquoted line)
    FossilOrigin-Name: a10659afb24dd60e0398487a45cf3574c365e29c2889fb70621565c850f845d4

--- debian_debug.sh
+++ debian_debug.sh
@@ -1,3 +1,4 @@
+export RUSTFLAGS="-C link-arg=-fuse-ld=lld"
 cargo build --target x86_64-unknown-linux-musl ; 
 cargo build ; 
 fossil uv add target/x86_64-unknown-linux-musl/debug/accept-connect --as a-c-x86_64-linux-musl-d; 
