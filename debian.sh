3e223dbb16e7db2173a6748af1a06b3741d08af8
rishi <oss@rishikeshan.com>
2024-02-28 02:45:23 +0000

    Change ld
    FossilOrigin-Name: afdca32216e5c55d78d95460b014b958cebcc51eb32581228304e0c1a1d4a976

--- debian.sh
+++ debian.sh
@@ -4,3 +4,4 @@ fossil uv add target/x86_64-unknown-linux-musl/release/accept-connect --as a-c-x
 fossil uv add target/x86_64-unknown-linux-musl/release/offer-listen --as o-l-x86_64-linux-musl; 
 fossil uv add target/release/accept-connect --as a-c-x86_64-linux-glibc-bookworm; 
 fossil uv add target/release/offer-listen --as o-l-x86_64-linux-glibc-bookworm
+export RUSTFLAGS=-C link-arg=-fuse-ld=lld
