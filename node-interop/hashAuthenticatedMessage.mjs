445b07c8419feea09546e3f1c1a99f049bad8d72
rishi <oss@rishikeshan.com>
2024-03-07 14:48:14 +0000

    [WIP] fix unaligned/unpadded byte added to the string in hex format.
    FossilOrigin-Name: 44fb40efbc5a79bc02c1483dc009c829ed2f2f5db03dcc9b01c984aad98c64e2

--- node-interop/hashAuthenticatedMessage.mjs
+++ node-interop/hashAuthenticatedMessage.mjs
@@ -17,24 +17,32 @@ export class hashAuthenticatedMessage{
 		this.MAC8 = await hmac.compute(this.#key8, this.Message, hashAlgo);
 		this.MAC="";
 		for(let i=0; i<this.MAC8.length; i++){
-			this.MAC+=this.MAC8[i].toString(16)
+			this.MAC+=this.MAC8[i].toString(16).padStart(2,0);
 		}
+		console.log(this.MAC8);
 		return this.MAC;
 	}
 	static async verifyAndReturn(message, key, hash){
 		let encoder = new TextEncoder();
 		let key8 = new Uint8Array(key.length);
+		let message8 = new Uint8Array(message.length);
+		let hashA = hash.split('');
 		let hash8tmp=[];
 		let b='';
-		while((b = hash.splice(0, 2)) !== []){
-			hash8tmp.push(b.parseInt(16));
+		console.log(hashA);
+		while(hashA.length !== 0){
+			let b = hashA.splice(0, 2);
+			hash8tmp.push(parseInt(b.join(''), 16));
 		}
-		message8 =  new Uint8Array(this.Message.length);
-		encoder.encodeInto(this.Message, this.message8);
+		message8 =  new Uint8Array(message.length);
+		encoder.encodeInto(message, message8);
 		let hash8 = new Uint8Array(hash8tmp);
 		encoder.encodeInto(key, key8);
-		result = await hmac.verify(key8, message8, hash8, hashAlgo);
-		if(result===false) throw new Error("MAC Error");
+		let result = await hmac.verify(key8, message8, hash8, hashAlgo);
+		console.log(key8);
+		console.log(message8);
+		console.log(hash8);
+		if(result===false) throw new Error(`MAC Error, expected: ${hash}, got ${hash}`);
 		else return result;
 	}
 }
--- node-interop/offer-listen.js
+++ node-interop/offer-listen.js
@@ -2,3 +2,10 @@ import {conf} from "./conf.mjs"
 import {timedMessage} from "./timedMessage.mjs"
 import {hashAuthenticatedMessage} from "./hashAuthenticatedMessage.mjs"
 
+let am = new hashAuthenticatedMessage('hello', 'hello');
+am.compute().then(console.log);
+am.compute().then(verify);
+console.log(am);
+function verify(result){
+hashAuthenticatedMessage.verifyAndReturn('hello', 'hello', result);
+}
