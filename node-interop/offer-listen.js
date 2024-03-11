9d4d9bcf032138ad9a13bf07b9f1a11752eb4a5e
rishi <oss@rishikeshan.com>
2024-03-11 13:43:54 +0000

    [WIP] Attempt at the UDP listener...
    FossilOrigin-Name: 36645dd79ef49e228da81950355dacadbabc6d6c8fe8ba2fb58015450045b3ef

--- node-interop/offer-listen.js
+++ node-interop/offer-listen.js
@@ -1,7 +1,13 @@
 import {conf} from "./conf.mjs"
 import {timedMessage} from "./timedMessage.mjs"
 import {hashAuthenticatedMessage} from "./hashAuthenticatedMessage.mjs"
+import {WebSocket} from 'ws'
+import * as dgram from 'dgram'
 
+console.assert(conf.PublishType == "wss");
+
+const selftest=true
+if(selftest){
 let am = new hashAuthenticatedMessage('hello', 'hello');
 am.compute().then(console.log);
 am.compute().then(verify);
@@ -9,3 +15,15 @@ console.log(am);
 function verify(result){
 hashAuthenticatedMessage.verifyAndReturn('hello', 'hello', result);
 }
+}
+let EndpointURL = conf.PublishEndpoint.split('//').slice(1).join('//')
+let wsurl = `wss://${conf.PublishAuthUser}:${conf.PublishAuthPass}@${EndpointURL}`
+console.log(wsurl)
+let sigSocket = new WebSocket(wsurl);
+sigSocket.addEventListener('message', (e) => console.log(e.data))
+let otherSocket = dgram.createSocket('udp6');
+let addrPortPair = `${conf.Address}:${conf.Port}`
+console.log(`Should listen on: ${addrPortPair}`)
+otherSocket.on('message', (msg, rinfo) => console.log)
+otherSocket.on('listening', () => console.log(`Listening on: ${JSON.stringify(otherSocket.address())}`))
+otherSocket.bind(conf.Port)
