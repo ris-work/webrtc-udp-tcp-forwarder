7f702d66c8d954d57c132afce6571aaba29b78bc
rishi <oss@rishikeshan.com>
2024-03-11 14:05:37 +0000

    [WIP]: detect v6 or v4, gets packets...
    FossilOrigin-Name: d1d9fcf3add632def210bfc1e3405e663a9a97d0c1c53723b65836b2d6fd0d49

--- node-interop/offer-listen.js
+++ node-interop/offer-listen.js
@@ -3,6 +3,7 @@ import {timedMessage} from "./timedMessage.mjs"
 import {hashAuthenticatedMessage} from "./hashAuthenticatedMessage.mjs"
 import {WebSocket} from 'ws'
 import * as dgram from 'dgram'
+import * as net from 'net'
 
 console.assert(conf.PublishType == "wss");
 
@@ -21,9 +22,16 @@ let wsurl = `wss://${conf.PublishAuthUser}:${conf.PublishAuthPass}@${EndpointURL
 console.log(wsurl)
 let sigSocket = new WebSocket(wsurl);
 sigSocket.addEventListener('message', (e) => console.log(e.data))
-let otherSocket = dgram.createSocket('udp6');
+let otherSocket;
+if(net.isIPv6(conf.Address)){
+otherSocket = dgram.createSocket('udp6');
+}
+else if (net.isIPv4(conf.Address)) {
+otherSocket = dgram.createSocket('udp4');
+}
+else{console.log("neither IPv4 nor 6")}
 let addrPortPair = `${conf.Address}:${conf.Port}`
 console.log(`Should listen on: ${addrPortPair}`)
-otherSocket.on('message', (msg, rinfo) => console.log)
+otherSocket.on('message', (msg, rinfo) => console.log(rinfo))
 otherSocket.on('listening', () => console.log(`Listening on: ${JSON.stringify(otherSocket.address())}`))
-otherSocket.bind(conf.Port)
+otherSocket.bind(conf.Port, conf.Address)
