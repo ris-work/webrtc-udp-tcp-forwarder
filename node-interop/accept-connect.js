295b47dfdbb70bffac5d7076342bf2dacea0c1f0
rishi <oss@rishikeshan.com>
2024-03-12 21:22:31 +0000

    [WIP]: got upto 'connectionstatechange'
    FossilOrigin-Name: 9eb3be3902a30c0d0f365f760f059f9f9ab97c2ef0f04ecf696df467adae2bd5

--- node-interop/accept-connect.js
+++ node-interop/accept-connect.js
@@ -3,6 +3,7 @@ import { timedMessage } from "./timedMessage.mjs";
 import { hashAuthenticatedMessage } from "./hashAuthenticatedMessage.mjs";
 import { WebSocket } from "ws";
 import wrtc from "wrtc";
+import * as process from "process";
 import * as dgram from "dgram";
 import * as net from "net";
 
@@ -10,6 +11,7 @@ console.assert(conf.WebRTCMode == "Accept");
 console.assert(conf.PublishType == "ws");
 
 let offerUnvalidated;
+let connected = false;
 
 const selftest = true;
 if (selftest) {
@@ -22,14 +24,22 @@ if (selftest) {
 	}
 }
 let EndpointURL = conf.PublishEndpoint.split("//").slice(1).join("//");
+
 let wsurl = `wss://${conf.PublishAuthUser}:${conf.PublishAuthPass}@${EndpointURL}`;
 console.log(wsurl);
 let sigSocket = new WebSocket(wsurl);
+sigSocket.addEventListener("close", (e) => {
+	if (!connected) process.exit(1);
+});
+sigSocket.addEventListener("open", (e) => {
+	proceedToWebRTC();
+});
 sigSocket.addEventListener("message", (e) => {
 	console.log(e.data);
 	offerUnvalidated = e.data;
 	gotOffer();
 });
+
 let otherSocket;
 if (net.isIPv6(conf.Address)) {
 	otherSocket = dgram.createSocket("udp6");
@@ -78,54 +88,65 @@ let dc_close = (x) => console.dir(x);
 let incoming_dc_message = (e) => console.dir(e);
 
 let pc, dc;
+let gotOffer;
 
-async function gotOffer() {
-	//let timeValidated = timedMessage.checkAndReturn(JSON.parse(offerUnvalidated));
-	let oU = JSON.parse(offerUnvalidated);
-	console.log(oU);
-	let hashValidated = await hashAuthenticatedMessage.verifyAndReturn(
-		oU.Message,
-		conf.PeerPSK,
-		oU.MAC
-	);
-	console.log({ hV: hashValidated });
-	let timeValidated = timedMessage.checkAndReturn(JSON.parse(hashValidated));
-	console.log({ tV: timeValidated });
-	pc = new wrtc.RTCPeerConnection(RTCConfig);
-	pc.addEventListener("connectionstatechange", pc_state_change);
-	pc.addEventListener("icegatheringerror", pc_ice_error);
-	pc.addEventListener("icegatheringstatechange", pc_ice_gathering_change);
-	pc.addEventListener("icecandidate", pc_ice_candidate);
+function proceedToWebRTC() {
+	gotOffer = async function () {
+		//let timeValidated = timedMessage.checkAndReturn(JSON.parse(offerUnvalidated));
+		let oU = JSON.parse(offerUnvalidated);
+		console.log(oU);
+		let hashValidated = await hashAuthenticatedMessage.verifyAndReturn(
+			oU.Message,
+			conf.PeerPSK,
+			oU.MAC
+		);
+		console.log({ hV: hashValidated });
+		let timeValidated = timedMessage.checkAndReturn(JSON.parse(hashValidated));
+		console.log({ tV: timeValidated });
+		pc = new wrtc.RTCPeerConnection(RTCConfig);
+		pc.addEventListener("connectionstatechange", pc_state_change);
+		pc.addEventListener("icegatheringerror", pc_ice_error);
+		pc.addEventListener("icegatheringstatechange", pc_ice_gathering_change);
+		pc.addEventListener("icecandidate", pc_ice_candidate);
 
-	/*dc = pc.createDataChannel('data');
+		/*dc = pc.createDataChannel('data');
 dc.addEventListener('message', incoming_dc_message);
 dc.addEventListener('open', dc_open);
 dc.addEventListener('close', dc_close);
 */
 
-	pc.setRemoteDescription(
-		new wrtc.RTCSessionDescription(JSON.parse(timeValidated))
-	);
-	//pc.addEventListener('negotiationneeded', pc_negotiation_needed);
-	//pc.setRemoteDescripotion
-	let answer = await pc.createAnswer();
-	pc.setLocalDescription(answer);
-	if (selftest)
-		setTimeout(
-			() =>
-				console.log("Gathered so far: " + JSON.stringify(pc.localDescription)),
-			8000
+		pc.setRemoteDescription(
+			new wrtc.RTCSessionDescription(JSON.parse(timeValidated))
 		);
-	//setTimeout(() => doneGeneratingOffer(JSON.stringify(pc.localDescription)), 5000)
-}
+		//pc.addEventListener('negotiationneeded', pc_negotiation_needed);
+		//pc.setRemoteDescripotion
+		let answer = await pc.createAnswer();
+		pc.setLocalDescription(answer);
+		if (selftest)
+			setTimeout(
+				() =>
+					console.log(
+						"Gathered so far: " + JSON.stringify(pc.localDescription)
+					),
+				8000
+			);
+		setTimeout(
+			() => doneGeneratingAnswer(JSON.stringify(pc.localDescription)),
+			2500
+		);
+	};
 
-function doneGeneratingANswer(offer) {
-	let timed = new timedMessage(offer);
-	let serializedTimed = JSON.stringify(timed);
-	let hmacMessage = new hashAuthenticatedMessage(serializedTimed, conf.PeerPSK);
-	hmacMessage.compute().then(() => sendOffer(hmacMessage));
-}
-function sendOffer(hmacMessage) {
-	console.log(hmacMessage);
-	sigSocket.send(JSON.stringify(hmacMessage));
+	function doneGeneratingAnswer(answer) {
+		let timed = new timedMessage(answer);
+		let serializedTimed = JSON.stringify(timed);
+		let hmacMessage = new hashAuthenticatedMessage(
+			serializedTimed,
+			conf.PeerPSK
+		);
+		hmacMessage.compute().then(() => sendAnswer(hmacMessage));
+	}
+	function sendAnswer(hmacMessage) {
+		console.log(hmacMessage);
+		sigSocket.send(JSON.stringify(hmacMessage));
+	}
 }
--- node-interop/offer-listen.js
+++ node-interop/offer-listen.js
@@ -4,10 +4,14 @@ import { hashAuthenticatedMessage } from "./hashAuthenticatedMessage.mjs";
 import { WebSocket } from "ws";
 import wrtc from "wrtc";
 import * as dgram from "dgram";
+import * as process from "process";
 import * as net from "net";
 
 console.assert(conf.WebRTCMode == "Offer");
 console.assert(conf.PublishType == "ws");
+
+let connected = false;
+let answerUnvalidated;
 
 const selftest = false;
 if (selftest) {
@@ -22,8 +26,20 @@ if (selftest) {
 let EndpointURL = conf.PublishEndpoint.split("//").slice(1).join("//");
 let wsurl = `wss://${conf.PublishAuthUser}:${conf.PublishAuthPass}@${EndpointURL}`;
 console.log(wsurl);
+
 let sigSocket = new WebSocket(wsurl);
-sigSocket.addEventListener("message", (e) => console.log(e.data));
+sigSocket.addEventListener("message", (e) => {
+	console.log(e.data);
+	answerUnvalidated = e.data;
+	gotAnswer();
+});
+sigSocket.addEventListener("close", (e) => {
+	if (!connected) process.exit(1);
+});
+sigSocket.addEventListener("open", (e) => {
+	proceedToWebRTC();
+});
+
 let otherSocket;
 if (net.isIPv6(conf.Address)) {
 	otherSocket = dgram.createSocket("udp6");
@@ -73,35 +89,57 @@ let dc_close = (x) => console.dir(x);
 let incoming_dc_message = (e) => console.dir(e);
 
 let pc = new wrtc.RTCPeerConnection(RTCConfig);
-pc.addEventListener("connectionstatechange", pc_state_change);
-pc.addEventListener("icegatheringerror", pc_ice_error);
-pc.addEventListener("icegatheringstatechange", pc_ice_gathering_change);
-pc.addEventListener("icecandidate", pc_ice_candidate);
+let gotAnswer;
+function proceedToWebRTC() {
+	pc.addEventListener("connectionstatechange", pc_state_change);
+	pc.addEventListener("icegatheringerror", pc_ice_error);
+	pc.addEventListener("icegatheringstatechange", pc_ice_gathering_change);
+	pc.addEventListener("icecandidate", pc_ice_candidate);
 
-let dc = pc.createDataChannel("data");
-dc.addEventListener("message", incoming_dc_message);
-dc.addEventListener("open", dc_open);
-dc.addEventListener("close", dc_close);
+	let dc = pc.createDataChannel("data");
+	dc.addEventListener("message", incoming_dc_message);
+	dc.addEventListener("open", dc_open);
+	dc.addEventListener("close", dc_close);
 
-pc.addEventListener("negotiationneeded", pc_negotiation_needed);
-if (selftest)
+	pc.addEventListener("negotiationneeded", pc_negotiation_needed);
+	if (selftest)
+		setTimeout(
+			() =>
+				console.log("Gathered so far: " + JSON.stringify(pc.localDescription)),
+			5000
+		);
 	setTimeout(
-		() =>
-			console.log("Gathered so far: " + JSON.stringify(pc.localDescription)),
-		5000
+		() => doneGeneratingOffer(JSON.stringify(pc.localDescription)),
+		2500
 	);
-setTimeout(
-	() => doneGeneratingOffer(JSON.stringify(pc.localDescription)),
-	3000
-);
 
-function doneGeneratingOffer(offer) {
-	let timed = new timedMessage(offer);
-	let serializedTimed = JSON.stringify(timed);
-	let hmacMessage = new hashAuthenticatedMessage(serializedTimed, conf.PeerPSK);
-	hmacMessage.compute().then(() => sendOffer(hmacMessage));
-}
-function sendOffer(hmacMessage) {
-	console.log(hmacMessage);
-	sigSocket.send(JSON.stringify(hmacMessage));
+	function doneGeneratingOffer(offer) {
+		let timed = new timedMessage(offer);
+		let serializedTimed = JSON.stringify(timed);
+		let hmacMessage = new hashAuthenticatedMessage(
+			serializedTimed,
+			conf.PeerPSK
+		);
+		hmacMessage.compute().then(() => sendOffer(hmacMessage));
+	}
+	function sendOffer(hmacMessage) {
+		console.log(hmacMessage);
+		sigSocket.send(JSON.stringify(hmacMessage));
+	}
+	gotAnswer = async function () {
+		//let timeValidated = timedMessage.checkAndReturn(JSON.parse(offerUnvalidated));
+		let aU = JSON.parse(answerUnvalidated);
+		console.log(aU);
+		let hashValidated = await hashAuthenticatedMessage.verifyAndReturn(
+			aU.Message,
+			conf.PeerPSK,
+			aU.MAC
+		);
+		console.log({ hV: hashValidated });
+		let timeValidated = timedMessage.checkAndReturn(JSON.parse(hashValidated));
+		console.log({ tV: timeValidated });
+		pc.setRemoteDescription(
+			new wrtc.RTCSessionDescription(JSON.parse(timeValidated))
+		);
+	};
 }
