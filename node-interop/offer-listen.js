6296e0c1e5312e0c3ab2e1efe84ddecd1291a551
rishi <oss@rishikeshan.com>
2024-03-12 12:07:41 +0000

    WIP: Transform uppercase keys to lowercase keys
    FossilOrigin-Name: 22a280beede64b030db2cfec57a612e9c40bfb1754b8b65aae2bd7d59ab50567

--- node-interop/offer-listen.js
+++ node-interop/offer-listen.js
@@ -2,6 +2,7 @@ import {conf} from "./conf.mjs"
 import {timedMessage} from "./timedMessage.mjs"
 import {hashAuthenticatedMessage} from "./hashAuthenticatedMessage.mjs"
 import {WebSocket} from 'ws'
+import wrtc from 'wrtc'
 import * as dgram from 'dgram'
 import * as net from 'net'
 
@@ -35,3 +36,35 @@ console.log(`Should listen on: ${addrPortPair}`)
 otherSocket.on('message', (msg, rinfo) => console.log(rinfo))
 otherSocket.on('listening', () => console.log(`Listening on: ${JSON.stringify(otherSocket.address())}`))
 otherSocket.bind(conf.Port, conf.Address)
+
+let transformedICEServers = [];
+for (const serverList in conf.ICEServers) {
+	let transformedServerList = {};
+	for(const key in conf.ICEServers[serverList]){
+		Object.defineProperty(transformedServerList, key.toLowerCase(), {value: conf.ICEServers[serverList][key], enumerable: true});
+		//console.log(conf.ICEServers[serverList][key]);
+		//console.log(key);
+		//console.log(transformedServerList);
+	}
+	//console.log("to:")
+	//console.dir(transformedServerList)
+	//console.log("from:")
+	//console.dir(conf.ICEServers[serverList])
+	transformedICEServers.push(transformedServerList);
+}
+const RTCConfig = {iceServers: transformedICEServers}
+console.log(JSON.stringify(RTCConfig))
+
+let pc_state_change = (x) => console.dir(x)
+
+let dc_open = (x) => console.dir(x)
+let dc_close = (x) => console.dir(x)
+let incoming_dc_message = (e) => console.dir(e)
+
+let pc = new wrtc.RTCPeerConnection();
+pc.addEventListener('connectionstatechange', pc_state_change);
+
+let dc = pc.createDataChannel('data');
+dc.addEventListener('message', incoming_dc_message);
+dc.addEventListener('open', dc_open);
+dc.addEventListener('close', dc_close);
