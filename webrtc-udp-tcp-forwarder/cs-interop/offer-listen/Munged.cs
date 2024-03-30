7a250098143daae7b3405103df780dd6180ab867
root <oss@rishikeshan.com>
2024-03-30 22:55:25 +0000

    dotnet format with LF only
    FossilOrigin-Name: 6e2003ca7d75f6f1942efc33fd1f180cb091d39e61ee4c0fbd2d29e09c849bc6

--- webrtc-udp-tcp-forwarder/cs-interop/offer-listen/Munged.cs
+++ webrtc-udp-tcp-forwarder/cs-interop/offer-listen/Munged.cs
@@ -5,9 +5,10 @@ using System.Xml.Serialization;
 
 namespace Rishi.Kexd;
 
-public class MungedSDP{
-    [JsonInclude] public required string sdp;
-    [JsonInclude] public required string type;
+public class MungedSDP
+{
+	[JsonInclude] public required string sdp;
+	[JsonInclude] public required string type;
 }
 
 [JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Metadata)]
