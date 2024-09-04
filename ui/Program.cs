af4b3e31d085fc3045947570102b5bb8c81bbe5b
Rishi <oss@rishikeshan.com>
2024-09-04 23:11:05 +0000

    Initial UI
    FossilOrigin-Name: bcd6bd7e650d1c707678570290a8351d4c496dda9a2ba75d25ad9d642811a694

---
+++ ui/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Terminal.Gui;
+using Tomlyn.Model;
+using Tomlyn;
+
+if (args.Length > 0)
+{
+    StartConfig.Filename = args[0];
+}
+Application.Run<RV.WebRTCForwarders.Window>();
+
+
+
+
+
+public static class StartConfig
+{
+    public static string Filename = "sample.toml";
+}
+
+public class Loader: Window
+{
+    public Loader() {
+        ColorScheme = new ColorScheme{ Normal = new Terminal.Gui.Attribute(Color.DarkGray, Color.Cyan) };
+        Title = $"Configuration Editor ({Application.QuitKey} to Quit)";
+        string TomlFileContents = System.IO.File.ReadAllText(StartConfig.Filename);
+        var model = Toml.ToModel(TomlFileContents);
+        var FileNameLabel = new Label() { Text = StartConfig.Filename };
+        var EndpointType = (string)model["Type"];
+        var EndpointTypeLabel = new Label() { Text = EndpointType };
+        Add(FileNameLabel, EndpointTypeLabel);
+    }
+}
