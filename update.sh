968dcd18cc61bc0fbe33a13d65bd292ac42f1eea
Rishikeshan S/L <oss@rishikeshan.com>
2024-10-04 16:30:12 +0200

    14:29:53 [50c2401d7d] *CURRENT* Update the update script (user: chromebook tags: nocc) \r\nWe had to do this because GitHub doesn't like .git directories in path

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 cd /home/chromebook/webrtc-udp-forwarder-git-export/webrtc-udp-tcp-forwarder
 /usr/bin/fossil update
-msg=$(/usr/bin/fossil timeline -n 1 | head -n 2)
+msg=$(/usr/bin/fossil timeline -n 1 | head -n -1 | tail -n -1)
 msg="$msg \r\nWe had to do this because GitHub doesn't like .git directories in path"
 echo $msg
 /usr/bin/git add -u
