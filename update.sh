5bcc2101c2514983a456024d8c8857990b9b5aa5
Rishikeshan S/L <oss@rishikeshan.com>
2024-10-04 16:34:22 +0200

    14:34:03 [e64e7ef6ac] *CURRENT* Better layout from update.sh (user: chromebook tags: nocc) We had to do this because GitHub doesn't like .git directories in path 😠😥

--- update.sh
+++ update.sh
@@ -3,7 +3,8 @@
 cd /home/chromebook/webrtc-udp-forwarder-git-export/webrtc-udp-tcp-forwarder
 /usr/bin/fossil update
 msg=$(/usr/bin/fossil timeline -n 1 | head -n -1 | tail -n -1)
-msg="$msg \r\nWe had to do this because GitHub doesn't like .git directories in path"
+msg="$msg
+We had to do this because GitHub doesn't like .git directories in path 😠😥"
 echo $msg
 /usr/bin/git add -u
 /usr/bin/git commit -m "$msg"
